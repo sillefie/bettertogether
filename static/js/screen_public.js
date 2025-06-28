d44dd885ca69c056581b682cfdb761ef786e7fad
Sillefie <s.celen@telenet.be>
2025-06-28 10:19:31 +0200

    Update 041

--- static/js/screen_public.js
+++ static/js/screen_public.js
@@ -17,6 +17,8 @@ function register() {
   currentScreen = "intro";
 }
 
+
+
 function vote(v) {
   ws.send(JSON.stringify({ type: "vote", vote: v }));v
   document.getElementById("intro").style.display = "none";
@@ -29,15 +31,42 @@ ws.onmessage = (event) => {
   if (msg.type === "screen") {
     if (msg.screen === "question") {
       document.getElementById("question").style.display = "";
-      document.getElementById("start").style.display = "none";
+      document.getElementById("intro").style.display = "none";
       document.getElementById("waiting").style.display = "none";
+      document.getElementById("feedback").style.display = "none";
+      document.getElementById("scoreboard").style.display = "none";
     } else {
       document.getElementById("question").style.display = "none";
     }
   } else if (msg.type === "question") {
     document.getElementById("questionText").innerText = msg.text;
     document.getElementById("question").style.display = "";
-    document.getElementById("start").style.display = "none";
     document.getElementById("waiting").style.display = "none";
- }
+    document.getElementById("feedback").style.display = "none";
+    document.getElementById("scoreboard").style.display = "none";
+      document.getElementById("intro").style.display = "none";
+ } else if (msg.type === "feedback") {
+    document.getElementById("question").style.display = "none";
+    document.getElementById("waiting").style.display = "none";
+    document.getElementById("feedback").style.display = "";
+
+    const playerName = localStorage.getItem("player_name");
+    const playerVote = msg.votes?.[playerName];
+    const coupleVote = msg.winning_name;
+
+    const feedbackText = document.getElementById("feedbackText");
+    feedbackText.innerText = "<h2>Bedankt om te stemmen!</h2><p>Wacht op het resultaat …</p>";
+  } else if (msg.type === "scoreboard") {
+      document.getElementById("intro").style.display = "none";
+    document.getElementById("feedback").style.display = "none";
+    document.getElementById("waiting").style.display = "none";
+    document.getElementById("scoreboard").style.display = "";
+    const list = document.getElementById("rankingList");
+    list.innerHTML = "";
+    msg.ranking.forEach(([name, score]) => {
+      const li = document.createElement("li");
+      li.textContent = '${name} - ${score}';
+      list.appendChild(li);
+    });
+  }
 };
